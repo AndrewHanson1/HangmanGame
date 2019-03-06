7c69482f06c36c778ff12b5927604e3bed7bd54e
unknown <andrew.lewis.hanson@gmail.com>
2019-03-05 17:20:35 -0700

    turn in

--- assets/javascriptfinal.js
+++ assets/javascriptfinal.js
@@ -1,7 +1,7 @@
 //create an array of words
 var words = ["ASH", "ASPEN", "HAWTHORN", "BIRCH", "HICKORY",
- "HORNBEAM", "COTTONWOOD", "ELM", "HEMLOCK", "BASSWOOD", "MAPLE", 
- "SPRUCE", "CEDAR", "OAK", "PINE", "SYCAMORE", "WILLOW", "BEECH", "BUTTERNUT", "CHERRY"]
+    "HORNBEAM", "COTTONWOOD", "ELM", "HEMLOCK", "BASSWOOD", "MAPLE",
+    "SPRUCE", "CEDAR", "OAK", "PINE", "SYCAMORE", "WILLOW", "BEECH", "BUTTERNUT", "CHERRY"]
 //html changes
 var underScoreHTML = document.getElementsByClassName("underScore");
 var winsHTML = document.getElementsByClassName("wins");
@@ -10,10 +10,12 @@ var myImage = document.querySelector('img');
 var chosen;
 var wins = 0;
 var letter;
+var arraySelector;
 var startingGuessAmount = 6;
 var guessesLeft = startingGuessAmount;
 var underScore = [];
-var buttonClass = document.querySelectorAll("li");
+var inputBlocker = false;
+var buttonClass = document.getElementsByTagName("li");
 var letterButtons = document.getElementsByClassName("letter");
 for (i = 0; i < letterButtons.length; i++) {
     letterButtons[i].addEventListener("click", grabButtonContent);
@@ -21,25 +23,27 @@ for (i = 0; i < letterButtons.length; i++) {
 }
 
 
+
 //choose word and genereate underscores and push to html
 function chooseWord() {
 
-    var arraySelector = Math.floor(Math.random() * words.length + 1);
+    arraySelector = Math.floor(Math.random() * words.length + 1);
     myImage.setAttribute("src", "assets/images/Trees/question.png");
     guessesLeft = startingGuessAmount;
     guessesLeftHTML[0].innerHTML = startingGuessAmount;
     chosen = words[arraySelector];
     underScore = [];
+    inputBlocker = false;
     for (i = 0; i < chosen.length; i++) {
 
         underScore.push('_');
     }
     underScoreHTML[0].innerHTML = underScore.join(" ");
-    for (j = 0; j <= buttonClass.length; j++) {
+    for (var j = 0; j <= buttonClass.length; j++) {
         buttonClass[j].setAttribute("class", "letter");
 
     }
-
+    
 }
 
 //getting inner html of button and processing it
@@ -53,105 +57,105 @@ function grabButtonContent() {
     }
 }
 //loss notification function
-function lossNotification(){
+function lossNotification() {
     underScoreHTML[0].innerHTML = "You lose! Try Again!"
 }
 // win notification
-function winNotification(){
+function winNotification() {
     underScoreHTML[0].innerHTML = "You Won! Play Again!"
 }
 //display word when they lose
-function lossDisplay(){
+function lossDisplay() {
     underScoreHTML[0].innerHTML = "The Word Was " + chosen;
 }
 //image changer
-function imageChanger(){
-    var number = Math.floor(Math.random() * words.length + 1);
-    switch(number){
-        case 0: 
-        myImage.setAttribute("src", "assets/images/Trees/Ash.jpg")
-        break;
-        
-        case 1: 
-        myImage.setAttribute("src", "assets/images/Trees/Aspen.jpg")
-        break;
+function imageChanger() {
+    var currentWord = words[arraySelector];
+    switch (currentWord) {
+        case "ASH":
+            myImage.setAttribute("src", "assets/images/Trees/Ash.jpg")
+            break;
 
-        case 3: 
-        myImage.setAttribute("src", "assets/images/Trees/Hawthorn.jpg")
-        break;
-        
-        case 4: 
-        myImage.setAttribute("src", "assets/images/Trees/Birch.jpg")
-        break;
+        case "ASPEN":
+            myImage.setAttribute("src", "assets/images/Trees/Aspen.jpg")
+            break;
 
-        case 5: 
-        myImage.setAttribute("src", "assets/images/Trees/Hickory.jpg")
-        break;
+        case "HAWTHORN":
+            myImage.setAttribute("src", "assets/images/Trees/Hawthorn.jpg")
+            break;
 
-        case 6: 
-        myImage.setAttribute("src", "assets/images/Trees/Hornbeam.jpg")
-        break;
+        case "BIRCH":
+            myImage.setAttribute("src", "assets/images/Trees/Birch.jpg")
+            break;
 
-        case 7: 
-        myImage.setAttribute("src", "assets/images/Trees/Cottonwood.jpg")
-        break;
+        case "HICKORY":
+            myImage.setAttribute("src", "assets/images/Trees/Hickory.jpg")
+            break;
 
-        case 8: 
-        myImage.setAttribute("src", "assets/images/Trees/Elm.jpg")
-        break;
+        case "HORNBEAM":
+            myImage.setAttribute("src", "assets/images/Trees/Hornbeam.jpg")
+            break;
 
-        case 9: 
-        myImage.setAttribute("src", "assets/images/Trees/Hemlock.jpg")
-        break;
+        case "COTTONWOOD":
+            myImage.setAttribute("src", "assets/images/Trees/Cottonwood.jpg")
+            break;
 
-        case 10: 
-        myImage.setAttribute("src", "assets/images/Trees/Basswood.jpg")
-        break;
+        case "ELM":
+            myImage.setAttribute("src", "assets/images/Trees/Elm.jpg")
+            break;
 
-        case 11: 
-        myImage.setAttribute("src", "assets/images/Trees/Maple.jpg")
-        break;
-        
-        case 12: 
-        myImage.setAttribute("src", "assets/images/Trees/Spruce.jpg")
-        break;
+        case "HEMLOCK":
+            myImage.setAttribute("src", "assets/images/Trees/Hemlock.jpg")
+            break;
 
-        case 13: 
-        myImage.setAttribute("src", "assets/images/Trees/Cedar.jpg")
-        break;
+        case "BASSWOOD":
+            myImage.setAttribute("src", "assets/images/Trees/Basswood.jpg")
+            break;
 
-        case 14: 
-        myImage.setAttribute("src", "assets/images/Trees/Oak.jpg")
-        break;
+        case "MAPLE":
+            myImage.setAttribute("src", "assets/images/Trees/Maple.jpg")
+            break;
 
-        case 15: 
-        myImage.setAttribute("src", "assets/images/Trees/Pine.jpg")
-        break;
+        case "SPRUCE":
+            myImage.setAttribute("src", "assets/images/Trees/Spruce.jpg")
+            break;
 
-        case 16: 
-        myImage.setAttribute("src", "assets/images/Trees/Sycamore.jpg")
-        break;
+        case "CEDAR":
+            myImage.setAttribute("src", "assets/images/Trees/Cedar.jpg")
+            break;
 
-        case 17: 
-        myImage.setAttribute("src", "assets/images/Trees/Willow.jpg")
-        break;
+        case "OAK":
+            myImage.setAttribute("src", "assets/images/Trees/Oak.jpg")
+            break;
 
-        case 18: 
-        myImage.setAttribute("src", "assets/images/Trees/Beech.jpg")
-        break;
+        case "PINE":
+            myImage.setAttribute("src", "assets/images/Trees/Pine.jpg")
+            break;
 
-        case 19: 
-        myImage.setAttribute("src", "assets/images/Trees/Butternut.jpg")
-        break;
+        case "SYCAMORE":
+            myImage.setAttribute("src", "assets/images/Trees/Sycamore.jpg")
+            break;
 
-        case 20: 
-        myImage.setAttribute("src", "assets/images/Trees/Cherry.png")
-        break;
+        case "WILLOW":
+            myImage.setAttribute("src", "assets/images/Trees/Willow.jpg")
+            break;
+
+        case "BEECH":
+            myImage.setAttribute("src", "assets/images/Trees/Beech.jpg")
+            break;
+
+        case "BUTTERNUT":
+            myImage.setAttribute("src", "assets/images/Trees/Butternut.jpg")
+            break;
+
+        case "CHERRY":
+            myImage.setAttribute("src", "assets/images/Trees/Cherry.png")
+            break;
 
 
-        
+
     }
-     
+
 }
 
 //game logic
@@ -162,12 +166,15 @@ function containsLetter(letterToCheck) {
             containsLetterBoolean = true;
             underScore[x] = letterToCheck;
             underScoreHTML[0].innerHTML = underScore.join(" ");
+            
 
         }
     }
-    if (containsLetterBoolean === false) {
-        guessesLeft--;
-        guessesLeftHTML[0].innerHTML = guessesLeft;
+    if (containsLetterBoolean === false && guessesLeft >= 1 && inputBlocker == false) {
+            guessesLeft--;
+            guessesLeftHTML[0].innerHTML = guessesLeft;
+        
+        
 
     }
 
@@ -180,12 +187,13 @@ function containsLetter(letterToCheck) {
 
 
 
-
+    
     else {
-        if (underScore.join("") == chosen) {
+        if (underScore.join("") == chosen && inputBlocker == false ) {
             wins++;
             guessesLeft = 8;
             winsHTML[0].innerHTML = wins;
+            inputBlocker = true;
             imageChanger();
             setTimeout(winNotification, 1000);
             setTimeout(chooseWord, 2500);
@@ -193,10 +201,14 @@ function containsLetter(letterToCheck) {
 
 
 
+
         }
 
     }
 }
+    
+
+//running game logic
 chooseWord();
 
 
